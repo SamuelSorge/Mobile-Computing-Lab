7cdfd14b6e16ee0ee8e745bd0efa06c065e459e4
SamuelSorge <jaasiel90@yahoo.de>
2015-05-12 10:51:41 +0200

    Enabled bluetooth in manifest (only BLE) Added bluetooth initialisation and corresponding imports

---
+++ Assignment_2/Task_1/Task_1/app/src/main/java/com/example/mobilecomputing/weatherapp/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.mobilecomputing.weatherapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothAdapter;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private BluetoothAdapter mBluetoothAdapter;
+    static final int REQUEST_ENABLE_BT = 1; // is this correct?
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+// displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
