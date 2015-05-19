f06fc5522f16155e9b1a140c8430c9fc31bfb299
Sebastian Hesse <sebi-hesse1@gmx.de>
2015-05-19 14:33:49 +0200

    formatting activity_main.xml; adding buttons and list view to show scanned devices; adds function to scan environment for bluetooth devices

--- Assignment_2/Task_1/Task_1/app/src/main/java/com/example/mobilecomputing/weatherapp/MainActivity.java
+++ Assignment_2/Task_1/Task_1/app/src/main/java/com/example/mobilecomputing/weatherapp/MainActivity.java
@@ -1,19 +1,43 @@
 package com.example.mobilecomputing.weatherapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity {
 
     private BluetoothAdapter mBluetoothAdapter;
-    static final int REQUEST_ENABLE_BT = 1; // is this correct?
+    private boolean mScanning;
+    private Handler mHandler;
+    private BluetoothDevice weatherDevice;
+    private ArrayAdapter<String> mNewDevicesArrayAdapter;
+    private static final long SCAN_PERIOD = 10000;
+    private static final int REQUEST_ENABLE_BT = 1; // is this correct?
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi,
+                                     byte[] scanRecord) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mNewDevicesArrayAdapter.add(device.getName() + "/" + device.getAddress());
+                        }
+                    });
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +50,15 @@ public class MainActivity extends ActionBarActivity {
         mBluetoothAdapter = bluetoothManager.getAdapter();
 
         // Ensures Bluetooth is available on the device and it is enabled. If not,
-// displays a dialog requesting user permission to enable Bluetooth.
+        // displays a dialog requesting user permission to enable Bluetooth.
         if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
+        mNewDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_main);
+
+        ListView newDevicesListView = (ListView) findViewById(R.id.deviceList);
+        newDevicesListView.setAdapter(mNewDevicesArrayAdapter);
     }
 
     @Override
@@ -54,4 +82,25 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void scanDevices(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+//            mBluetoothAdapter.startLeScan(new UUID[] {UUID.fromString("00000002-0000-0000-fdfd-fdfdfdfdfdfd")}, mLeScanCallback);
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+    }
+
 }
