1f56239af48938a9fb346fbb94fba1d04f9f980f
Sebastian Hesse <sebi-hesse1@gmx.de>
2015-06-09 14:09:14 +0200

    added task 4

--- iBeaconTriangulation/app/src/main/java/com/example/mobilecomputing/ibeacontriangulation/app/MainActivity.java
+++ iBeaconTriangulation/app/src/main/java/com/example/mobilecomputing/ibeacontriangulation/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.mobilecomputing.ibeacon.app;
+package com.example.mobilecomputing.ibeacontriangulation.app;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
@@ -11,6 +11,7 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.*;
 
@@ -24,6 +25,13 @@ public class MainActivity extends Activity {
     private BluetoothAdapter mBluetoothAdapter;
     private Handler mHandler = new Handler();
     private boolean mScanning;
+    private List<CoordPoint> points = new ArrayList<>(3);
+
+    {
+        points.add(new CoordPoint("00:1A:7D:DA:71:13", 0, 0));
+        points.add(new CoordPoint("00:1A:7D:DA:71:07", 1, 0));
+        points.add(new CoordPoint("5C:F3:70:61:43:C8", 0, 6));
+    }
 
     private BluetoothAdapter.LeScanCallback mLeScanCallback =
             new BluetoothAdapter.LeScanCallback() {
@@ -44,6 +52,7 @@ public class MainActivity extends Activity {
                                 }
                                 advertisements.add(advertisement);
                             } catch (Exception e) {
+                                e.printStackTrace();
                                 System.out.println("Error.");
                             }
                         }
@@ -93,6 +102,7 @@ public class MainActivity extends Activity {
      * Fills the device list with information to device, distance and numbers of received advertisements.
      */
     private void generateDeviceList() {
+        Map<String, Double> distances = new HashMap<>();
         for (BluetoothDevice device : beacons.keySet()) {
             List<Advertisement> advertisements = beacons.get(device);
             String prefix = "";
@@ -108,6 +118,8 @@ public class MainActivity extends Activity {
                 minor = ad.minor;
             }
             double calculatedDistance = distanceSum / advertisements.size();
+            distances.put(device.getAddress(), calculatedDistance);
+
             StringBuilder deviceDetails = new StringBuilder();
             deviceDetails.append("device: ").append(device.getName());
             deviceDetails.append(", device address: ").append(device.getAddress());
@@ -117,8 +129,14 @@ public class MainActivity extends Activity {
                     .append(", minor: ").append(minor);
             deviceDetails.append(", distance: ").append(calculatedDistance);
             deviceDetails.append(", advertisements: ").append(advertisements.size());
+
             mDevicesArrayAdapter.add(deviceDetails.toString());
         }
+
+        CoordPoint position = getLocationByTrilateration(points.get(0), distances.get(points.get(0).getDeviceAddress()),
+                points.get(1), distances.get(points.get(1).getDeviceAddress()), points.get(2), distances.get(points.get(2).getDeviceAddress()));
+        TextView devicePosition = (TextView) findViewById(R.id.devicePosition);
+        devicePosition.setText("X: "+position.getX()+", Y: "+position.getY());
     }
 
     private String convertByteToHex(final byte[] bytes) {
@@ -187,6 +205,154 @@ public class MainActivity extends Activity {
         this.beacons.clear();
     }
 
+    public CoordPoint getLocationByTrilateration(
+            CoordPoint point1, double distance1,
+            CoordPoint point2, double distance2,
+            CoordPoint point3, double distance3){
+
+        //DECLARACAO DE VARIAVEIS
+        CoordPoint result = new CoordPoint("ANDROID");
+        double[] P1   = new double[2];
+        double[] P2   = new double[2];
+        double[] P3   = new double[2];
+        double[] ex   = new double[2];
+        double[] ey   = new double[2];
+        double[] p3p1 = new double[2];
+        double jval  = 0;
+        double temp  = 0;
+        double ival  = 0;
+        double p3p1i = 0;
+        double triptx;
+        double xval;
+        double yval;
+        double t1;
+        double t2;
+        double t3;
+        double t;
+        double exx;
+        double d;
+        double eyy;
+
+        //TRANSFORMA OS PONTOS EM VETORES
+        //PONTO 1
+        P1[0] = point1.getX();
+        P1[1] = point1.getY();
+        //PONTO 2
+        P2[0] = point2.getX();
+        P2[1] = point2.getY();
+        //PONTO 3
+        P3[0] = point3.getX();
+        P3[1] = point3.getY();
+
+        //TRANSFORMA O VALOR DE METROS PARA A UNIDADE DO MAPA
+        //DISTANCIA ENTRE O PONTO 1 E A MINHA LOCALIZACAO
+        distance1 = (distance1 / 100000);
+        //DISTANCIA ENTRE O PONTO 2 E A MINHA LOCALIZACAO
+        distance2 = (distance2 / 100000);
+        //DISTANCIA ENTRE O PONTO 3 E A MINHA LOCALIZACAO
+        distance3 = (distance3 / 100000);
+
+        for (int i = 0; i < P1.length; i++) {
+            t1   = P2[i];
+            t2   = P1[i];
+            t    = t1 - t2;
+            temp += (t*t);
+        }
+        d = Math.sqrt(temp);
+        for (int i = 0; i < P1.length; i++) {
+            t1    = P2[i];
+            t2    = P1[i];
+            exx   = (t1 - t2)/(Math.sqrt(temp));
+            ex[i] = exx;
+        }
+        for (int i = 0; i < P3.length; i++) {
+            t1      = P3[i];
+            t2      = P1[i];
+            t3      = t1 - t2;
+            p3p1[i] = t3;
+        }
+        for (int i = 0; i < ex.length; i++) {
+            t1 = ex[i];
+            t2 = p3p1[i];
+            ival += (t1*t2);
+        }
+        for (int  i = 0; i < P3.length; i++) {
+            t1 = P3[i];
+            t2 = P1[i];
+            t3 = ex[i] * ival;
+            t  = t1 - t2 -t3;
+            p3p1i += (t*t);
+        }
+        for (int i = 0; i < P3.length; i++) {
+            t1 = P3[i];
+            t2 = P1[i];
+            t3 = ex[i] * ival;
+            eyy = (t1 - t2 - t3)/Math.sqrt(p3p1i);
+            ey[i] = eyy;
+        }
+        for (int i = 0; i < ey.length; i++) {
+            t1 = ey[i];
+            t2 = p3p1[i];
+            jval += (t1*t2);
+        }
+        xval = (Math.pow(distance1, 2) - Math.pow(distance2, 2) + Math.pow(d, 2))/(2*d);
+        yval = ((Math.pow(distance1, 2) - Math.pow(distance3, 2) + Math.pow(ival, 2) + Math.pow(jval, 2))/(2*jval)) - ((ival/jval)*xval);
+
+        t1 = point1.getX();
+        t2 = ex[0] * xval;
+        t3 = ey[0] * yval;
+        triptx = t1 + t2 + t3;
+        result.setX(triptx);
+        t1 = point1.getY();
+        t2 = ex[1] * xval;
+        t3 = ey[1] * yval;
+        triptx = t1 + t2 + t3;
+        result.setY(triptx);
+
+        return result;
+    }
+
+    class CoordPoint {
+
+        private String deviceAddress;
+        private double x;
+        private double y;
+
+        public CoordPoint(final String deviceAddress) {
+            this.deviceAddress = deviceAddress;
+        }
+
+        public CoordPoint(final String deviceAddress, final double x, final double y) {
+            this.deviceAddress = deviceAddress;
+            this.x = x;
+            this.y = y;
+        }
+
+        public String getDeviceAddress() {
+            return deviceAddress;
+        }
+
+        public void setDeviceAddress(final String deviceAddress) {
+            this.deviceAddress = deviceAddress;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public void setX(final double x) {
+            this.x = x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setY(final double y) {
+            this.y = y;
+        }
+    }
+
     /**
      * Contains attributes of received advertisement.
      */
@@ -226,3 +392,4 @@ public class MainActivity extends Activity {
         }
     }
 }
+
