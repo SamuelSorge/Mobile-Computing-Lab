2ada9115b0c0bc21111150dcff347387d56ab823
SamuelSorge <jaasiel90@yahoo.de>
2015-07-12 21:11:07 +0200

    Added check if own message is received, TODO: forwarding, etc

--- Assignment_4/Ad-hocCommunication/Flooding/src/com/mobileComputingAssignment4/Main.java
+++ Assignment_4/Ad-hocCommunication/Flooding/src/com/mobileComputingAssignment4/Main.java
@@ -2,41 +2,126 @@ package com.mobileComputingAssignment4;
 
 import java.io.IOException;
 import java.net.*;
+import java.text.Collator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-public class Main {
+public class Main
+{
+    private static DatagramSocket recvSock ;
+    private static DatagramSocket ackSock ;
+    private static DatagramSocket sendSock;
 
-    private static DatagramSocket senderSock;
-    private static DatagramSocket receiverSock;
-    private static DatagramPacket senderPacket;
-    private static DatagramPacket receiverPacket;
+    private static DatagramPacket recvPacket;
+    private static DatagramPacket ackPacket;
+    private static DatagramPacket sendPacket;
+
     private static final int teamNumber = 5;
     private static final int port = 5000 + 10 * teamNumber;
-    private static String msg;
+
     private static List<InetAddress> nodes = new ArrayList<InetAddress>();
+    private static List<String> nodeAddresses = new ArrayList<String>();
 
+    private static String ackMsg = "";
+    private static String bcastMsg = "";
 
-    private static int i = 1;
+    public static void main(String[] args) throws IOException
+    {
+        byte[] recvBuf = new byte[100]; // used to store received message data
+        byte[] ackBuf = new byte[100]; // used to store ack message data
+        byte[] sendBuf = new byte[100];
 
-    private static boolean initializeSocket(DatagramSocket sock) {
-        try {
-            sock.setBroadcast(true);
+        // Open Socket to listen to all UDP traffic on this port
+        try
+        {
+            recvSock = new DatagramSocket(port);
+            recvSock.setBroadcast(true);
         } catch (SocketException e) {
             e.printStackTrace();
-            return false;
         }
-        System.out.println("Sender socket initialized");
-        return true;
+
+        // Open Socket to send acknowledgements
+        try
+        {
+            ackSock = new DatagramSocket();
+            ackSock.setBroadcast(false);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        // Open Socket to send broadcast
+        try
+        {
+            sendSock = new DatagramSocket();
+            sendSock.setBroadcast(true);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Sockets initialized");
+
+        // Create new receiver packet to store received message data
+        recvPacket = new DatagramPacket(recvBuf, recvBuf.length);
+
+        bcastMsg = args[0];
+        sendBuf = bcastMsg.getBytes();
+        System.out.println("Message initialized with: "+ bcastMsg);
+
+        sendPacket = new DatagramPacket(sendBuf, sendBuf.length, InetAddress.getByName("192.168.132.255"), port);
+
+        int i = 0;
+
+        while (i < 20)
+        {
+            System.out.println("Sending UDP message ");
+            sendSock.send(sendPacket);
+
+            System.out.println("Receiving UDP messages ");
+            try
+            {
+                recvBuf = null;
+                // Receive a packet
+                recvSock.receive(recvPacket);
+
+                // Print received packet data
+                System.out.println("Packet received from: " + recvPacket.getAddress().getHostAddress());
+                System.out.println("Received packet data: " + new String(recvPacket.getData()));
+
+                String test = new String(recvPacket.getData());
+                Collator collator = Collator.getInstance(Locale.US);
+                System.out.println("Convert: " + collator.compare(test, bcastMsg));
+
+                //Send a response
+                if (0 == collator.compare(test, bcastMsg))
+                {
+                    System.out.println("Received own broadcast message");
+                }
+                else
+                {
+                   System.out.println("Received another broadcast message");
+                    ackMsg = "Received your Hello World";
+                    ackBuf = ackMsg.getBytes();
+                    ackPacket = new DatagramPacket(ackBuf, ackBuf.length, recvPacket.getAddress(), recvPacket.getPort());
+                    ackSock.send(ackPacket);
+                    System.out.println("Ack sent to: " + ackPacket.getAddress().getHostAddress());
+                }
+
+                //nodes.add(receiverPacket.getAddress());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            i++;
+        }
     }
 
-
-
-    public static void main(String[] args) throws UnknownHostException {
+    /*public static void main(String[] args) throws UnknownHostException {
 	// write your code here
         byte[] bcast_msg;
-        byte[] rec_msg = new byte[200];
-        byte[] buf = new byte[100];
+
+        byte[] buf = new byte[100]; // used to store received message data
+
 
         try {
             senderSock = new DatagramSocket();
@@ -44,11 +129,18 @@ public class Main {
             e.printStackTrace();
         }
         initializeSocket (senderSock);
-        try {
+
+
+        try
+        {
+            // Open Socket to listen to all UDP traffic on this port
             receiverSock = new DatagramSocket(port);
         } catch (SocketException e) {
             e.printStackTrace();
         }
+
+        // Enable Broadcast on this socket and port
+        initializeSocket(receiverSock);
 
 
 
@@ -92,5 +184,5 @@ public class Main {
             System.out.println("Address: " + nodes.get(j));
         }
 
-    }
+    }*/
 }
