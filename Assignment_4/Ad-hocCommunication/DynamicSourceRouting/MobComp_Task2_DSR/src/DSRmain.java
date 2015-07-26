96702e826fcaaae384a78e83610b17a66807606f
Sebastian Hesse <sebi-hesse1@gmx.de>
2015-07-26 18:14:49 +0200

    fixed compile errors

--- Assignment_4/Ad-hocCommunication/DynamicSourceRouting/MobComp_Task2_DSR/src/DSRmain.java
+++ Assignment_4/Ad-hocCommunication/DynamicSourceRouting/MobComp_Task2_DSR/src/DSRmain.java
@@ -1,7 +1,5 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.Socket;
-import java.net.SocketException;
 
 
 public class DSRmain {
@@ -15,16 +13,16 @@ public class DSRmain {
 	public static void main(String[] args) {
 		//Socket socket;
 		Server server;
-		try {
-			server = new Server (socket);
-			Client client = new Client(socket, server);
-
-			server.start();
-			client.start();
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			server = new Server (socket);
+//			Client client = new Client(socket, server);
+//
+//			server.start();
+//			client.start();
+//		} catch (SocketException e) {
+//			 TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	
 
 	}
--- Assignment_4/Ad-hocCommunication/DynamicSourceRouting/MobComp_Task2_DSR/src/Server.java
+++ Assignment_4/Ad-hocCommunication/DynamicSourceRouting/MobComp_Task2_DSR/src/Server.java
@@ -1,4 +1,3 @@
-^
 import java.net.Socket;
 import java.net.SocketException;
 
